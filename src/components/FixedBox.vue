b40b4e95a7896ed66e33d8579a85abc01b21ea93
xsze <xie_iron@163.com>
2018-09-02 22:11:46 +0800

    ...

--- src/components/FixedBox.vue
+++ src/components/FixedBox.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
         <div
+          :id="id"
           :style="css"
           v-on:touchstart="dosth"
           v-on:touchmove="move"
+          v-on:touchend="slide"
           ></div>
     </div>
 </template>
@@ -12,10 +14,14 @@ export default {
   name: 'FixedBox',
   data () {
     return {
+      id:"fix",
       left: 100,
       top: 100,
       x: 0,
-      y: 0
+      y: 0,
+      ox: 0,
+      oy: 0,
+      ot: 0
     }
   },
   computed: {
@@ -27,8 +33,9 @@ export default {
   },
   methods: {
     dosth (e) {
-      this.x = e.touches[0].screenX
-      this.y = e.touches[0].screenY
+      this.x = this.ox = e.touches[0].screenX
+      this.y = this.oy = e.touches[0].screenY
+      this.ot = new Date().getTime()
     },
     move (e) {
       e.preventDefault()
@@ -36,6 +43,56 @@ export default {
       this.left += e.touches[0].screenX - this.x
       this.x = e.touches[0].screenX
       this.y = e.touches[0].screenY
+    },
+    slide (e) {
+      const startSlideTime = new Date().getTime()
+      let totalSlideTime=0 // ms
+      const dt = (startSlideTime - this.ot) / 1000
+      const dx = this.x - this.ox
+      const dy = this.y - this.oy
+      this.ox = this.x
+      this.oy = this.y
+      const dd = Math.sqrt(dx*dx+dy*dy)
+      let g = -100 // 加速度
+      let gx = dx / dd * g // x加速度
+      let gy = dy / dd * g // y加速度
+      let sx = dx / dt
+      let sy = dy / dt
+      let ix = 1
+      let iy = 1
+      const duraTime = (0 - sx) / gx * 1000 //
+      const that = this
+      const time = 1 / 60
+      const h=this.$el.querySelector('#' + that.id).clientHeight
+      const w=that.$el.querySelector('#' + that.id).clientWidth
+      console.log(dx, dy, sx, sy)
+      if (dt > 0.1 && (dx || dy)) {
+      const Iid=setInterval(function(){
+        console.log("interval",totalSlideTime,duraTime)
+        if (totalSlideTime >= duraTime) {
+          clearInterval(Iid)
+          return
+          }
+        // console.log(that.top,that.left,gy,gx)
+        totalSlideTime += time * 1000
+        that.x = (sx * time + gx * time * time / 2) * ix
+        that.y = (sy * time + gy * time * time / 2) * iy
+        that.left += that.x
+        that.top += that.y
+        if(that.top<=0 ||
+        that.top + h>=window.innerHeight){
+          iy=-iy
+        }
+        if(that.left<=0 ||
+        that.left + w>=window.innerWidth){
+          ix=-ix
+        }
+        sx = gx * time + sx
+        sy = gy * time + sy
+      },
+      time * 1000
+      )
+      }
     }
   }
 }
